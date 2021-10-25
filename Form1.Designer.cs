4077aeee52232492174785979317bff27802c955
Alexey-Gaidai <71748686+Alexey-Gaidai@users.noreply.github.com>
2021-10-25 20:32:40 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Laba3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 123);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(239, 391);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(273, 123);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "Points";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Legend = "Legend1";
+            series2.Name = "Line";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series3.Legend = "Legend1";
+            series3.Name = "Spline";
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(562, 391);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(597, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(226, 90);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(835, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.очиститьToolStripMenuItem.Text = "Очистить ";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 520);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 28);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Рандомить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(273, 520);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 28);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Загрузить из Sheets";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(512, 520);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(175, 28);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Загрузить из XLS";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 554);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(175, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(74, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 596);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Sheets.v4;
+using org.mariuszgromada.math.mxparser;
+
+namespace Laba3
+{
+    public partial class Form1 : Form
+    {
+
+        public static List<point> steps = new List<point>();//список точек
+
+        private static readonly string[] Scopes = { SheetsService.Scope.Spreadsheets };
+        private const string SpreadsheetId = "1Kcvpqi-I6wY0HSFGehgdVp_tS70Fk2KQroZT39Z8S5Q";
+        private const string GoogleCredentialsFileName = "google-credentials.json";
+        private const string ReadRange = "Лист1!A:B";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /*private double f(double x)//вынес подставления значения в функцию в отдельный метод
+        {
+            double result = 0;
+            Function f = new Function("f(x) = " + textBox1.Text);
+            string sklt = "f()";
+            string fx = sklt.Insert(2, x.ToString());
+            fx = fx.Replace(",", ".");
+            Expression fxx = new Expression(fx, f);
+            result = fxx.calculate();
+            return result;
+        }*/
+
+        public void MathPart()
+        {
+            double sumX = 0;
+            double SumY = 0;
+            double SumXY = 0;
+            double X2 = 0;
+            double Y2 = 0;
+            double count = steps.Count;
+            double a, b;
+
+            foreach(var p in steps)
+            {
+                sumX += p.x;
+                SumY += p.y;
+                SumXY += p.x * p.y;
+                X2 += p.x * p.x;
+                Y2 += p.y * p.y;
+            }
+            a = Math.Round(LinearRegerssionA(sumX, SumY, SumXY, X2, Y2, count), 4);
+            b = Math.Round(LinearRegerssionB(sumX, SumY, SumXY, X2, Y2, count), 4);
+
+            string func = "f(x) = " + a + "*x+" + b;
+            Function f = new Function(func);
+            label1.Text = func;
+        }
+
+        public double LinearRegerssionA(double sumX, double SumY, double SumXY, double X2, double Y2, double count)
+        {
+            double a;
+            a = (sumX * SumY - count * SumXY) / ((sumX * sumX) - count * X2);
+            return a;
+        }
+
+        public double LinearRegerssionB(double sumX, double SumY, double SumXY, double X2, double Y2, double count)
+        {
+            double b;
+            b = (sumX * SumXY - X2 * SumY) / ((sumX * sumX) - count * X2);
+            return b;
+        }
+
+        public void graph()
+        {
+
+        }
+
+        public void randompoints(double n)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int value1 = rnd.Next(0, 100);
+                int value2 = rnd.Next(0, 100);
+                point abc = new point(value1, value2);
+                steps.Add(abc);
+
+            }
+
+        }
+        public void addgraphpoints()
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double count = Convert.ToDouble(textBox1.Text);
+            randompoints(count);
+            foreach (var p in steps)
+            {
+                dataGridView1.Rows.Add(p.x, p.y);
+                chart1.Series[0].Points.AddXY(p.x, p.y);
+            }
+
+        }
+
+        async private void button3_Click(object sender, EventArgs e)
+        {
+            var serviceValues = GetSheetsService().Spreadsheets.Values;
+            await ReadAsync(serviceValues);
+            foreach (var p in steps)
+            {
+                dataGridView1.Rows.Add(p.x, p.y);
+                chart1.Series[0].Points.AddXY(p.x, p.y);
+            }
+        }
+
+        private static SheetsService GetSheetsService()
+        {
+            using (var stream = new FileStream(GoogleCredentialsFileName, FileMode.Open, FileAccess.Read))
+            {
+                var serviceInitializer = new BaseClientService.Initializer
+                {
+                    HttpClientInitializer = GoogleCredential.FromStream(stream).CreateScoped(Scopes)
+                };
+                return new SheetsService(serviceInitializer);
+            }
+        }
+
+        private async Task ReadAsync(SpreadsheetsResource.ValuesResource valuesResource)
+        {
+            var response = await valuesResource.Get(SpreadsheetId, ReadRange).ExecuteAsync();
+            var values = response.Values;
+            if (values == null || !values.Any())
+            {
+                 Console.WriteLine("No data found.");
+                 return;
+            }
+            for (int i = 0; i < values.Count; i++)
+            {
+                double val0 = Convert.ToDouble(values[i][0]);
+                double val1 = Convert.ToDouble(values[i][1]);
+                point aaa = new point(val0, val1);
+                steps.Add(aaa);
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MathPart();
+        }
+    }
+
+    public class point//класс для сохранения точек
+    {
+        public double x, y;
+        public point(double X, double Y)
+        {
+            this.x = X;
+            this.y = Y;
+        }
+    }
+}
